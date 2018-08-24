25a7ca43b4fadbe0de310635426ce472f9ebd298
erisalke <erisalke@gmail.com>
2018-08-24 21:55:40 +0200

    Add mongo

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
 import Task from './Task.jsx';
-
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
-  }
+import { Tasks } from '../api/tasks.js';
  
+
+class App extends Component {
   renderTasks() {
-    return this.getTasks().map((task) => (
+    return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -30,3 +25,9 @@ export default class App extends Component {
     );
   }
 }
+
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}).fetch()
+  };
+})(App);
