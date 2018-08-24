df9f40fe8ffe8ae194aeaf164d546c167c0c8664
erisalke <erisalke@gmail.com>
2018-08-24 22:22:43 +0200

    Add open task coutner

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,7 +8,7 @@ import { Tasks } from '../api/tasks.js';
 class App extends Component {
   constructor(props) {
     super(props);
- 
+
     this.state = {
       hideCompleted: false,
     };
@@ -47,7 +47,7 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
 
           <label className="hide-completed">
             <input
@@ -78,6 +78,7 @@ class App extends Component {
 
 export default withTracker(() => {
   return {
-    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch()
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 })(App);
