a006ce6bf3178821412039fec863b5e934dd06d7
erisalke <erisalke@gmail.com>
2018-08-24 22:08:05 +0200

    Add form to add new tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 import Task from './Task.jsx';
 import { Tasks } from '../api/tasks.js';
- 
+
 
 class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+ 
+    const text = findDOMNode(this.refs.textInput).value.trim();
+ 
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+ 
+    findDOMNode(this.refs.textInput).value = '';
+  }
+
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
- 
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={(ev) => this.handleSubmit(ev)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
- 
+
         <ul>
           {this.renderTasks()}
         </ul>
@@ -28,6 +50,6 @@ class App extends Component {
 
 export default withTracker(() => {
   return {
-    tasks: Tasks.find({}).fetch()
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch()
   };
 })(App);
