17f30b126bb2ee25e0d8873040e32c0fb8ba6ad9
erisalke <erisalke@gmail.com>
2018-08-27 00:47:00 +0200

    Display messages in the chat room

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
 import AccountsUIWrapper from './others/AccountsUIWrapper.jsx';
 import Menu from './Menu/Menu.jsx';
 import ChatRoom from './ChatRoom.jsx';
 
 export default class App extends Component {
   render() {
+    const ChatRoomWithRouter = withRouter(props => <ChatRoom {...props}/>);
+
     return (
       <div className="wrapper">
         <header>
@@ -18,7 +21,7 @@ export default class App extends Component {
           </div>
 
           <div className="chat-room">
-            <ChatRoom />
+            <ChatRoomWithRouter />
           </div>
         </div>
       </div>
--- imports/ui/ChatRoom.jsx
+++ imports/ui/ChatRoom.jsx
@@ -1,35 +1,65 @@
 import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Messages } from '/imports/api/messages.js';
 
-export default class ChatList extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      messages: [
-        { id: 0, sender: 'Bob', text: 'hi' },
-        { id: 1, sender: 'Ann', text: 'hello' },
-        { id: 2, sender: 'Bob', text: 'how are you?' }
-      ],
-    };
+class ChatList extends Component {
+  sendMessage(event) {
+    event.preventDefault();
+ 
+    const text = findDOMNode(this.refs.textInput).value.trim();
+    const roomId = this.props.match.params.id;
+    Meteor.call('messages.send', text, roomId);
+    
+    findDOMNode(this.refs.textInput).value = '';
   }
 
   renderChatRooms() {
-    let messages = this.state.messages || [];
+    const roomId = this.props.match.params.id;
+
+    // this should be done in selector
+    const messages = this.props.messages
+      .filter(msg => msg.room === roomId);
 
     return messages.map((msg) => {
       return (
-        <li key={msg.id} >
+        <li key={msg._id} >
           <strong>{msg.sender}</strong>: {msg.text}
         </li>
       );
     });
   }
 
+  renderInputBox() {
+    return (
+      <form className="new-message" onSubmit={this.sendMessage.bind(this)} >
+        <input
+          type="text"
+          ref="textInput"
+          placeholder="Type to send message"
+        />
+      </form>
+    );
+  }
+
   render() {
     return (
-      <ul>
-        {this.renderChatRooms()}
-      </ul>
+      <div>
+        <ul>
+          {this.renderChatRooms()}
+        </ul>
+        {this.renderInputBox()}
+      </div>
     );
   }
 }
+
+export default withTracker(() => {
+  Meteor.subscribe('messages');
+
+  return {
+    messages: Messages.find({}).fetch(),
+  };
+})(ChatList);
