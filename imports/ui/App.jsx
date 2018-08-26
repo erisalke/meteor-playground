2adc2d602939c69024c244c0b522a155393fb0da
erisalke <erisalke@gmail.com>
2018-08-27 00:58:41 +0200

    Disable writing for no room selected

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import AccountsUIWrapper from './others/AccountsUIWrapper.jsx';
 import Menu from './Menu/Menu.jsx';
 import ChatRoom from './ChatRoom.jsx';
 
 export default class App extends Component {
   render() {
-    const ChatRoomWithRouter = withRouter(props => <ChatRoom {...props}/>);
+    const ChatRoomWithRouter = withRouter(props => <ChatRoom {...props} />);
 
     return (
       <div className="wrapper">
@@ -21,7 +21,11 @@ export default class App extends Component {
           </div>
 
           <div className="chat-room">
-            <ChatRoomWithRouter />
+            {
+              this.props.match.params.id
+                ? <ChatRoomWithRouter />
+                : <div>Please select a chat room</div>
+            }
           </div>
         </div>
       </div>
